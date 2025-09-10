4fcf46afcd214ae5b3c8b84865655af6665078e3
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-09-10 11:28:56 +0300

    refactor: update dice probability calculation and improve output formatting

--- JS-examples/Module 1/t10/t10.js
+++ JS-examples/Module 1/t10/t10.js
@@ -1,14 +1,31 @@
 'use strict';
 const target = document.querySelector('#target');
+console.log(target);
 
 const numOfDice = prompt('Enter number of dice');
 
-const sumOfDice = prompt('Enter sum');
+const sumOfDice = +prompt('Enter sum');
+
+let count = 0;
+for (let i = 0; i < 10000; i++) {
+  let sum = 0;
+  for (let j = 0; j < numOfDice; j++) {
+    const dice = Math.floor(Math.random() * 6) + 1;
+    sum = sum + dice;
+  }
+  //   console.log('Sum of dice', sum);
+  //   console.log(sumOfDice);
+  if (sum === sumOfDice) {
+    count++;
+  }
+}
+// console.log(count);
+const probability = (count / 100).toFixed(2);
 
 const html = `
 <b>Number of dice:</b> ${numOfDice}
 <br>
-<b>Sum of dice: </b> ${sumOfDice}
+Probability of ${sumOfDice} is ${probability}%
 `;
 
 target.innerHTML = html;
