4dd397cf6d64dfbfb54444673e5a92878e639ce4
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-10-01 11:28:20 +0300

    feat: implement restaurantModal function to display restaurant details and courses in a modal

--- JS-examples/ajax/t5/components.js
+++ JS-examples/ajax/t5/components.js
@@ -13,4 +13,51 @@ const restaurantRow = (restaurant) => {
   return tr;
 };
 
-export { restaurantRow };
+const restaurantModal = (restaurant, googleMaps) => {
+  const { name, address, postalCode, city, phone, company } = restaurant;
+  let restaurantHtml = `
+    <div class="dialog-head">
+        <h1>${name}</h1>
+
+        <button type="button">❌</button>
+    </div>
+
+    <p>
+    Address: <a href="${googleMaps(restaurant)}">${address}</a><br>
+    Postal code: ${postalCode}<br>
+    City: ${city}<br>
+    Phone number: <a href="tel:${phone}">${phone}</a><br>
+    Company: ${company}
+    </p>
+  `;
+
+  let coursesHtml = `
+  <table>
+    <thead>
+        <tr>
+            <th>
+                Name
+            </th>
+            <th>
+                Price
+            </th>
+            <th>
+                Diet
+            </th>
+        </tr>
+    </thead>
+    <tbody>`;
+  for (const { name, price, diets } of courses) {
+    coursesHtml += `
+    <tr>
+        <td>${name}</td>
+        <td>${price || 'no price definded'}</td>
+        <td>${diets || 'no diets definded'}</td>
+    </tr>`;
+  }
+  coursesHtml += '</tbody>';
+
+  return restaurantHtml + coursesHtml;
+};
+
+export { restaurantRow, restaurantModal };
--- JS-examples/ajax/t5/t5.js
+++ JS-examples/ajax/t5/t5.js
@@ -1,5 +1,5 @@
 'use strict';
-import { restaurantRow } from './components.js';
+import { restaurantModal, restaurantRow } from './components.js';
 import { fetchData } from './utils/commonFunctions.js';
 import { apiUrl } from './utils/variables.js';
 
@@ -36,56 +36,10 @@ const googleMaps = (restaurant) => {
 };
 
 const showModal = (restaurant, courses) => {
-  const { name, address, postalCode, city, phone, company } = restaurant;
-  let coursesHtml = `
-  <table>
-    <thead>
-        <tr>
-            <th>
-                Name
-            </th>
-            <th>
-                Price
-            </th>
-            <th>
-                Diet
-            </th>
-        </tr>
-    </thead>
-    <tbody>`;
-  for (const { name, price, diets } of courses) {
-    coursesHtml += `
-    <tr>
-        <td>${name}</td>
-        <td>${price || 'no price definded'}</td>
-        <td>${diets || 'no diets definded'}</td>
-    </tr>`;
-  }
-  coursesHtml += '</tbody>';
-
   const dialog = document.querySelector('dialog');
-
-  // use showModal() method, as .open = true; or .show() opens the modal in the bottom of the page
-  // "JavaScript should be used to display the <dialog> element. Use the .showModal() method to display a modal dialog and the .show() method to display a non-modal dialog."
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dialog
   dialog.showModal();
 
-  dialog.innerHTML = `
-  <div class="dialog-head">
-    <h1>${name}</h1>
-
-    <button type="button">❌</button>
-  </div>
-
-  <p>
-  Address: <a href="${googleMaps(restaurant)}">${address}</a><br>
-  Postal code: ${postalCode}<br>
-  City: ${city}<br>
-  Phone number: <a href="tel:${phone}">${phone}</a><br>
-  Company: ${company}
-  </p>
-  ${coursesHtml}
-  `;
+  dialog.innerHTML = restaurantModal(restaurant, googleMaps);
 
   dialog.querySelector('button').addEventListener('click', () => {
     dialog.close();
